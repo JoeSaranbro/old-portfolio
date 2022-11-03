f484c69ba7f3e4b51b38e52b882b13df64a9b81a
JoeSaranbro <108979915+JoeSaranbro@users.noreply.github.com>
2022-11-03 21:11:08 +0700

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ const Login = ( {setUser} ) => {
 
 
   return (
-    <div className='bg-black'>
+    <div className='flex bg-black'>
     <section className='section'>
       <form className='form' onSubmit={handleSubmit}>
         <h5>login</h5>
